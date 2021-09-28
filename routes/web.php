f9b6d8d0b938d0e941230f931341f11c3b36a65c
Nick Koenig <nick.koenig.be@gmail.com>
2021-09-28 08:34:00 +0200

    feat(app) add dummy routes + views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+
+Route::get('/', function () {
+    return view('homepage');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/collection', function () {
+    return view('collection');
+});
+
+Route::get('/detail', function () {
+    return view('detail');
 });
