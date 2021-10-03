6ceb91aa77cf27864f36d887ca172d946a3d66c3
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-10-03 17:01:06 +0200

    completed the seeders for the other teammates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,11 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call([
-            // UserSeeder::class,
-            // CollectionSeeder::class,
+            UserSeeder::class,
+            CollectionSeeder::class
         ]);
 
-        // \App\Models\Nft::factory(10)->create();
+        \App\Models\Nft::factory(10)->create();
         \App\Models\Comment::factory(20)->create();
     }
 }
