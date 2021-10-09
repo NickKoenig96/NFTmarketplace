62d3c08490ea29c6066a73ad67747c1b16e80ea7
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-10-09 15:31:55 +0200

    made a layout for the profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,19 @@
-<h1>Profile</h1>
+@extends('layouts/profile')
 
-<img src="images/{{ $user->avatar }}" alt="avatar">
-<p>{{$user->name}}</p>
+
+@section('content')
+    <h1>Profile</h1>
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <img src="../images/{{$user->avatar}}" alt="avatar">
+                <p>{{$user->name}}</p>
+            </div>
+        </div>
+    </div>
+@endsection
+
 
