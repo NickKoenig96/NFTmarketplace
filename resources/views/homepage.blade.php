5f9614cd483b6c48b431a9f0347fdfa3d3e2f7af
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2021-10-15 15:58:38 +0200

    adds navbar to homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,8 +1,15 @@
-<h1>Homepage</h1>
+@extends('layouts/app')
+@section('title', 'Home')
 
-@foreach ($nfts as $nft)
-    <div>
-        <a href="/nfts/{{$nft->id}}">{{ $nft->title }}</a>
-        {{-- add image --}}
-    </div>
-@endforeach
+@section('content')
+    <x-header firstname="{{ 'Jonathan' }}" />
+    <h1>Homepage</h1>
+
+    @foreach ($nfts as $nft)
+        <div>
+            <a href="/nfts/{{$nft->id}}">{{ $nft->title }}</a>
+            {{-- add image --}}
+        </div>
+    @endforeach
+
+@endsection
