a8b96b5203d339d8af88cf06ca7934e21f4dd1ad
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-10-15 16:37:09 +0200

    set image in command

--- config/app.php
+++ config/app.php
@@ -161,11 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-<<<<<<< HEAD
-        Intervention\Image\ImageServiceProvider::class
-=======
-        Intervention\Image\ImageServiceProvider::class,
->>>>>>> 3d9fcf84e0dc1a542e1a19d26a1967b80ce92583
+        // Intervention\Image\ImageServiceProvider::class
 
         /*
          * Package Service Providers...
@@ -233,12 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 3d9fcf84e0dc1a542e1a19d26a1967b80ce92583
+        // 'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
