7872c9a007abdeb4006053df09c63de617340d17
Nick Koenig <nick.koenig.be@gmail.com>
2021-10-16 10:40:52 +0200

    fix(config) remove image identifier classes

--- config/app.php
+++ config/app.php
@@ -161,11 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-<<<<<<< HEAD
-        Intervention\Image\ImageServiceProvider::class
-=======
-        Intervention\Image\ImageServiceProvider::class,
->>>>>>> 3d9fcf84e0dc1a542e1a19d26a1967b80ce92583
 
         /*
          * Package Service Providers...
@@ -233,12 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 3d9fcf84e0dc1a542e1a19d26a1967b80ce92583
 
     ],
 
