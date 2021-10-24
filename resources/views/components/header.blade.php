12b3e3b344e51077638efbad0a2e16b159f2ba5f
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2021-10-24 14:21:01 +0200

    feat: Makes navbar functional

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class=search>
-        <img src="{{ url('assets/atria_logo.svg') }}" alt="Logo Atria">
+        <a href="/"><img src="{{ url('assets/atria_logo.svg') }}" alt="Logo Atria"></a>
         <form action="{{ url('/search') }}" type="get" autocomplete="off">
             <select name="category" id="category">
                 <option value="Collections">Collections</option>
@@ -15,10 +15,10 @@
     </div>
 
     <nav>
-        <a href="#">My wallets</a>
-        <a href="#">My favourites</a>
+        <a href="/wallet">My wallets</a>
+        <a href="/profile">My favourites</a>
         <a href="#">About us</a>
         <a href="#">Contact</a>
-        <a class="btn btn--blue btn--h40 btn--signedin" href="#">{{ $firstname }}</a>
+        <a class="btn btn--blue btn--h40 btn--signedin" href="/profile">{{ $firstname }}</a>
     </nav>
 </header>
