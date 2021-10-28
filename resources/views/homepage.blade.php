f436a5cb80f37ae0d4070ced220caef81d77f68e
Nick Koenig <nick.koenig.be@gmail.com>
2021-10-28 08:30:39 +0200

    feat(wallet) add styling wallet

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,23 +9,23 @@
     <x-header firstname="{{ $user->firstname }}" />
 
     {{-- <h1>Homepage</h1> --}}
-    
+
     <section>
         <p>1 euro = {{ $eth }}ETH</p>
         <h1>Collections</h1>
 
         <div class="cardgallery">
             @foreach ($collections as $collection)
-            <a class="card card--3col" href="/collections/{{ $collection->id }}">
-                <img class="card__image" src="{{ $collection->image_file_path}}" alt="collection image">
-                <img class="card__profilepicture--small" src="{{ $collection->image_file_path }}" alt="creator image">
-                <div class="card__specs">
-                    <!-- <div class="btn--favourite"></div> -->
-                    <div class="btn--nftcount"><span>5</span></div>
-                </div>
-                <p class="card__title ta_c" style="margin-bottom: 12px;">{{ $collection->title }}</p>
-                <p class="card__description body--normal"> {{ $collection->description }}</p>
-            </a>
+                <a class="card card--3col" href="/collections/{{ $collection->id }}">
+                    <img class="card__image" src="{{ $collection->image_file_path }}" alt="collection image">
+                    <img class="card__profilepicture--small" src="{{ $collection->image_file_path }}" alt="creator image">
+                    <div class="card__specs">
+                        <!-- <div class="btn--favourite"></div> -->
+                        <div class="btn--nftcount"><span>5</span></div>
+                    </div>
+                    <p class="card__title ta_c" style="margin-bottom: 12px;">{{ $collection->title }}</p>
+                    <p class="card__description body--normal"> {{ $collection->description }}</p>
+                </a>
             @endforeach
         </div>
     </section>
@@ -34,7 +34,7 @@
         <h1>NFT's</h1>
         <form action="{{ url('/homepageFilter') }}" type="get">
             <h2 class="form-group__title">Filter:</h2>
-            <select name="filter" id="filter"> 
+            <select name="filter" id="filter">
                 <option value="Price">Price</option>
                 <option value="Area">Area</option>
                 <option value="Type">Type</option>
@@ -47,11 +47,14 @@
                 <div class="card card--3col flex--spbet">
                     <img src="{{ $nft->image_file_path }}" alt="nft image" class="card__image card__image--large">
                     <div class="marginb-24">
-                    <div class="flex--spbet">
-                        <p class="card__title" style="margin-bottom: 0px;">{{ $nft->title }}</p>
-                        <div class="btn--favourite"></div>
-                    </div>
+                        <div class="flex--spbet">
+                            <p class="card__title" style="margin-bottom: 0px;">{{ $nft->title }}</p>
+                            <div class="btn--favourite"></div>
+                        </div>
                         <span class="card__price">€ {{ $nft->price }}</span>
+                        <br>
+                        <span class="card__price">ETH {{ $eth * $nft->price }}</span>
+
                     </div>
                     <div class="flex--spbet">
                         <a href="/nfts/{{ $nft->id }}" class="btn btn--light btn--1col">View</a>
--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -9,33 +9,42 @@
 
     <section>
         <h1>wallet</h1>
+        <a href="/nft/addNft" class="btn btn--blue btn--155">add NFT</a>
 
-        <h2>Your collections (at the moment all collections)</h2>
+        <br>
+        <div class="cardgallery">
+            @foreach ($nfts as $nft)
+                @if ($nft->owner_id == $user->id)
 
-        @foreach ($collections as $collection)
-            <div>
-                <p>id = {{ $collection->id }}</p>
-                <p>{{ $collection->title }}</p>
-                <a href="delete/{{ $collection->id }}">DELETE</a>
-                <a href="edit/{{ $collection->id }}">EDIT</a>
+                    <div class="card card--3col flex--spbet">
+                        <img src="{{ $nft->image_file_path }}" alt="nft image" class="card__image card__image--large">
+                        <div class="marginb-24">
+                            <div class="flex--spbet">
+                                <p class="card__title" style="margin-bottom: 0px;">{{ $nft->title }}</p>
+                                <div class="btn--favourite"></div>
+                            </div>
+                            <span class="card__price">€ {{ $nft->price }}</span>
+                            <br>
+                            <span class="card__price">ETH {{ $eth * $nft->price }}</span>
 
-            </div>
+                        </div>
+                        <div class="flex--spbet">
+                            <a href="/nfts/{{ $nft->id }}" class="btn btn--light btn--1col">View</a>
+                            <a href="/nfts/{{ $nft->id }}" class="btn btn--blue btn--155">Buy</a>
 
-        @endforeach
+                        </div>
+                        <br>
+                        <div class="flex--spbet">
+                            <a href="delete/nft/{{ $nft->id }}" class="btn btn--red btn--1col">DELETE</a>
+                            <a href="edit/nft/{{ $nft->id }}" class="btn btn--green btn--1col">EDIT</a>
 
-        <h2>Your NFT's (at the moment all NFT's)</h2>
+                        </div>
+                    </div>
+                @endif
 
-        @foreach ($nfts as $nft)
-            <div>
-                <p>id = {{ $nft->id }}</p>
-                <p>{{ $nft->title }}</p>
-                <a href="delete/nft/{{ $nft->id }}">DELETE</a>
-                <a href="edit/nft/{{ $nft->id }}">EDIT</a>
+            @endforeach
+        </div>
 
-            </div>
+    </section>
 
-        @endforeach
-
-        </section>
-        
 @endsection
