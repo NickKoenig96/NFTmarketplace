cd7de406398395a883fbeaa3677426fb710fb39e
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-10-28 22:35:50 +0200

    new faker data for nft

--- database/factories/NftFactory.php
+++ database/factories/NftFactory.php
@@ -28,7 +28,7 @@ class NftFactory extends Factory
             'owner_id' => $this->faker->numberBetween(1, 4),
             'description' => $this->faker->realText(100, 2),
             'collection_id' => $this->faker->numberBetween(1, 5),
-            'owner_id' => $this->faker->numberBetween(3, 5),
+            'forSale' => $this->faker->numberBetween(0,1),
             'image_file_path' => $this->faker->randomElement(['https://res.cloudinary.com/dqelbnq5n/image/upload/v1635444084/Alderaan_planeet_hepk4w.webp', 'https://res.cloudinary.com/dqelbnq5n/image/upload/v1635444165/Mustafar-TROSGG_vzpuru.webp', 'https://res.cloudinary.com/dqelbnq5n/image/upload/v1635444245/Hoth_AoRCR_skga2j.webp']),
             'price' => $this->faker->numberBetween(75, 400),
             'object_type' => $this->faker->name(),
