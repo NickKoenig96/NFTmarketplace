203c984df5ed3ef1ed2967d5beb5190088ec7e4f
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-11-03 17:34:40 +0100

    removed views from collection

--- resources/views/collection/showCollection.blade.php
+++ resources/views/collection/showCollection.blade.php
@@ -18,7 +18,7 @@
                     <h1>{{ $collection->title }}</h1>
                     <h4 class="blue--20">Creator: {{ $collection->creator->firstname . " " . $collection->creator->lastname }}</h4>
                     <div class="margint-12 flex flex--alcen">
-                        <div class="btn--view"></div><h5 class="blue--20 marginr-48 marginl-12">11k Views</h5>
+                        <!-- <div class="btn--view"></div><h5 class="blue--20 marginr-48 marginl-12">11k Views</h5> -->
                         <!-- <a href="#" class="btn--favourite btn--favourite--small"></a><h5 class="blue--20 marginl-12">favourite</h5> -->
                     </div>
                     
