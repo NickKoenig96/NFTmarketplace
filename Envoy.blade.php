b25006001accee63ae034b485ee5a8040de257de
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-11-29 15:25:59 +0100

    add path to key on server

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,12 +1,12 @@
-@server[['development' => '-i path /hacker@139.162.134.152 ']];
+@server[['development' => '-i /home/jonathan/.ssh/authorized_keys hacker@139.162.134.152 ']];
 
 @task('deploy', ['on' => 'development'])
-cd /home/hacker/beta
-php artisan down
-git reset --hard HEAD
-git pull origin master
-php composer.phar install
-php composer.phar dump-autoload
-php artisan migrate --force
-php artisan up
+    cd /home/hacker/beta
+    php artisan down
+    git reset --hard HEAD
+    git pull origin master
+    php composer.phar install
+    php composer.phar dump-autoload
+    php artisan migrate --force
+    php artisan up
 @endtask 
