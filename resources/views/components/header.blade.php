c6e7176320966ec4f00a1474746fadea5a6a827c
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-12-19 17:18:08 +0100

    fixed search layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class=search>
         <a href="/"><img src="{{ url('assets/atria_logo.svg') }}" alt="Logo Atria"></a>
-        <form action="{{ url('/search') }}" type="get" autocomplete="off">
+        <form class="header" action="{{ url('/search') }}" type="get" autocomplete="off">
             <select name="category" id="category">
                 <option value="Collections">Collections</option>
                 <option value="NFTs">NFT's</option>
@@ -17,8 +17,6 @@
     <nav>
         <a href="/wallet">My wallet</a>
         <a href="/profile/#favorite">My favourites</a>
-        <a href="#">About us</a>
-        <a href="#">Contact</a>
         <a class="btn btn--blue btn--h40 btn--signedin" href="/profile">{{ $firstname }}</a>
 
     </nav>
